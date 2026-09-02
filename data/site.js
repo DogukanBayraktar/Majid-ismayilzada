834244482ec50c33cc8b32e086880b132b3e1b7c
DogukanBayraktar <77547230+DogukanBayraktar@users.noreply.github.com>
2026-09-02 15:02:16 +0300

    Yönetim panelinden site ayarları güncellendi

--- data/site.js
+++ data/site.js
@@ -7,7 +7,7 @@ const siteSettings = {
   "whatsapp": "905015804482",
   "whatsappLink": "https://wa.me/905015804482",
   "email": "info@majidismayilzada.com",
-  "address": "Cumhuriyet, Halaskargazi Cd. No:121, Şişli/İstanbul",
+  "address": "Cumhuriyet, Halaskargazi Cd. No:121, Şişli/İstanbul kadıköy",
   "mapsLink": "https://www.google.com/maps?q=Halaskargazi+Cd.+No%3A121,+%C5%9Ei%C5%9Fli/%C4%B0stanbul",
   "instagram": "https://www.instagram.com/dr.mecid.ismayilzade/",
   "ctaAppointmentText": "Randevu Al",
